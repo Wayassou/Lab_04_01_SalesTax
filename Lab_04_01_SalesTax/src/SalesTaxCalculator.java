bd17e94829526ecd9998d79b342805cf1b5dc41a
Wayassou <wayassou@gmail.com>
2024-10-29 22:26:05 -0400

    Add files via upload

---
+++ Lab_04_01_SalesTax/src/SalesTaxCalculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SalesTaxCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the purchase price: ");
+        double purchasePrice = input.nextDouble();
+        double salesTax = purchasePrice * 0.05;
+        System.out.println("Purchase Price: $" + purchasePrice);
+        System.out.println("Sales Tax (5%): $" + salesTax);
+    }
+}
